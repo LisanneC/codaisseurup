069a14e59c8b2a63c0a37ca061211b596091b51e
lisanne <lisanne.chin@gmail.com>
2017-07-05 19:16:54 +0200

    Add Model Specs: Events

--- app/models/event.rb
+++ app/models/event.rb
@@ -11,4 +11,12 @@ class Event < ApplicationRecord
 
   has_and_belongs_to_many :categories
 
+  def cheapevent?
+    price < 30
+  end
+
+  def self.order_by_price
+  order :price
+  end
+
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #describe "validations" do
+    #it { is_expected.to validate_presence_of(:name) }
+    #it { is_expected.to validate_length_of(:description).is_at_most(500) }
+    #it { is_expected.to validate_presence_of(:location) }
+  #end
+
+  #describe "#cheapevent?" do
+    #let(:cheapevent_event) { create :event, price: 20 }
+    #let(:non_cheapevent_event) { create :event, price: 200 }
+
+    #it "returns true if the price is smaller than 30 EUR" do
+      #expect(cheapevent_event.cheapevent?).to eq(true)
+      #expect(non_cheapevent_event.cheapevent?).to eq(false)
+    #end
+  #end
+
+  describe ".order_by_price" do
+    let!(:event1) { create :event, price: 100 }
+    let!(:event2) { create :event, price: 200 }
+    let!(:event3) { create :event, price: 50 }
+
+    it "returns a sorted array of events by prices" do
+      # note that they should not come out in the order that they were created
+      expect(Event.order_by_price).to eq([event3, event1, event2])
+    end
+  end
+
 end
